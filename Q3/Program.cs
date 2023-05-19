706196415fef68431ebb3d0e3ab1a62770c0cca1
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2023-05-19 09:33:48 +0700

    first commit

---
+++ Q3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int charCount, digitCount;
+        string product;
+        Dictionary<string, string> products = new Dictionary<string, string>();
+
+        Console.Write("Enter the number of English characters: ");
+        charCount = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of digits: ");
+        digitCount = int.Parse(Console.ReadLine());
+
+        string firstCode = GenerateFirstCode(charCount, digitCount);
+
+        Console.WriteLine("Enter product names (type 'Stop' to stop):");
+
+        while (true)
+        {
+            product = Console.ReadLine();
+            if (product == "Stop")
+                break;
+
+            string code = GenerateNextCode(products.Count, charCount, digitCount);
+            products.Add(code, product);
+        }
+
+        Console.Write("Enter a product code to search: ");
+        string searchCode = Console.ReadLine();
+
+        if (products.ContainsKey(searchCode))
+        {
+            string productName = products[searchCode];
+            Console.WriteLine("Product found: " + productName);
+        }
+        else
+        {
+            Console.WriteLine("Product not found!");
+        }
+    }
+
+    static string GenerateFirstCode(int charCount, int digitCount)
+    {
+        string firstCode = new string('A', charCount) + new string('0', digitCount);
+        return firstCode;
+    }
+
+    static string GenerateNextCode(int index, int charCount, int digitCount)
+    {
+        int totalCodes = (charCount + 1) * (int)Math.Pow(10, digitCount + 1);
+        if (index >= totalCodes)
+            throw new Exception("Cannot generate code. All possible codes have been assigned.");
+
+        string code = "";
+        int charIndex = index / (int)Math.Pow(10, digitCount + 1);
+        int digitIndex = index % (int)Math.Pow(10, digitCount + 1);
+
+        for (int i = 0; i < charCount; i++)
+        {
+            char c = (char)('A' + charIndex % 26);
+            code = c + code;
+            charIndex /= 26;
+        }
+
+        code += digitIndex.ToString().PadLeft(digitCount, '0');
+
+        return code;
+    }
+}
---
+++ Q4/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 1;
+        int columns = 1;
+        Stack<Tuple<int, int>> undoStack = new Stack<Tuple<int, int>>();
+        Stack<Tuple<int, int>> redoStack = new Stack<Tuple<int, int>>();
+
+        while (true)
+        {
+            int move = int.Parse(Console.ReadLine());
+
+            if (move == 11)
+            {
+                break;
+            }
+            else if (move == 9 && undoStack.Count > 0)
+            {
+                Tuple<int, int> temp = new Tuple<int, int>(rows, columns);
+                Tuple<int, int> previousMove = undoStack.Pop();
+                rows = previousMove.Item1;
+                columns = previousMove.Item2;
+                redoStack.Push(temp);
+                continue;
+            }
+            else if (move == 10 && redoStack.Count > 0)
+            {
+                Tuple<int, int> temp = new Tuple<int, int>(rows, columns);
+                Tuple<int, int> nextMove = redoStack.Pop();
+                rows = nextMove.Item1;
+                columns = nextMove.Item2;
+                undoStack.Push(temp);
+                continue;
+            }
+            else if (move >= 1 && move <= 8)
+            {
+                int x = int.Parse(Console.ReadLine());
+                bool canMove = CheckMove(move, rows, columns, x);
+
+                if (canMove)
+                {
+                    Tuple<int, int> temp = new Tuple<int, int>(rows, columns);
+                    undoStack.Push(temp);
+                    Move(move, ref rows, ref columns, x);
+                    redoStack.Clear();
+                }
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Invalid move. Please try again.");
+            }
+        }
+
+        Console.WriteLine($"{Convert.ToChar(columns - 1 + 'A')} {rows}");
+    }
+
+    static bool CheckMove(int action, int rows, int columns, int x)
+    {
+        switch (action)
+        {
+            case 1:
+                return rows + x <= 8;
+            case 2:
+                return rows + x <= 8 && columns - x >= 1;
+            case 3:
+                return columns - x >= 1;
+            case 4:
+                return rows - x >= 1 && columns - x >= 1;
+            case 5:
+                return rows - x >= 1;
+            case 6:
+                return rows - x >= 1 && columns + x <= 8;
+            case 7:
+                return columns + x <= 8;
+            case 8:
+                return rows + x <= 8 && columns + x <= 8;
+            default:
+                return false;
+        }
+    }
+
+    static void Move(int action, ref int rows, ref int columns, int x)
+    {
+        switch (action)
+        {
+            case 1:
+                rows += x;
+                break;
+            case 2:
+                rows += x;
+                columns -= x;
+                break;
+            case 3:
+                columns -= x;
+                break;
+            case 4:
+                rows -= x;
+                columns -= x;
+                break;
+            case 5:
+                rows -= x;
+                break;
+            case 6:
+                rows -= x;
+                columns += x;
+                break;
+            case 7:
+                columns += x;
+                break;
+            case 8:
+                rows += x;
+                columns += x;
+                break;
+        }
+    }
+}
